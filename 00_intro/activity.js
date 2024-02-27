2b1c34a1ce0fc5c7bd381923705ae58c6d017e78
igna <ignacioborraz@hotmail.com>
2024-02-26 22:27:53 -0300

    00_intro

---
+++ 00_intro/activity.js
@@ -0,0 +1,17 @@
+const nombre = "ignacio";
+let edad = 33;
+const precio = 10;
+const nombredeSerie = "stranger things";
+const peliculas = ["pokemon", "superman", "los caballeros del zodiaco"];
+
+console.log(nombre);
+console.log(edad);
+console.log(precio);
+console.log(nombredeSerie);
+console.log(peliculas);
+
+edad = edad + 1;
+edad += 1;
+edad++;
+
+console.log(edad);
