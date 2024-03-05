4bf4257ca25a322e602039ec79a918bb865c7d1b
igna <ignacioborraz@hotmail.com>
2024-03-04 23:00:20 -0300

    02_ec

--- 02_ec/NotesManager.js
+++ 02_ec/NotesManager.js
@@ -1,9 +1,9 @@
 class NotesManager {
-  static quantity = 0;
-  static #notes = [];
-  create(data) {
-    const note = {
-      id:
+  static quantity = 0;    //propiedad de la clase
+  static #notes = [];     //propiedad privada de la clase
+  create(data) {          //método del gestor que crea una nota (y la agrega al array de notas)
+    const note = {        //primero construye el objeto nota
+      id:                 //buscar la forma de que el id sea numerico y autoincremental
         NotesManager.quantity === 0
           ? 1
           : NotesManager.#notes[NotesManager.quantity - 1].id + 1,
@@ -20,11 +20,11 @@ class NotesManager {
   }
 }
 
-const notes = new NotesManager();
-notes.create({ text: "mi primera nota" });
+const notes = new NotesManager();             //defino la instancia del gestor
+notes.create({ text: "mi primera nota" });    //creo tres notas nota
 notes.create({ text: "mi segunda nota" });
 notes.create({ text: "mi tercera nota" });
-notes.create({});
+notes.create({});                             //intento crear una nota sin la propiedad text
 
 //console.log(NotesManager.#notes);
-console.log(notes.read());
+console.log(notes.read());                    //busco todas las notas y las muestro por consola
