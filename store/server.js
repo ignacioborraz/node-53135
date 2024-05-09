0d4c17045dc2379c323c46ead798e69878942a8c
igna <ignacioborraz@hotmail.com>
2024-05-09 19:42:20 -0300

    paginate

--- store/server.js
+++ store/server.js
@@ -1,6 +1,9 @@
 import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import sessions from "express-session";
+import MongoStore from "connect-mongo";
 
 import indexRouter from "./src/routers/index.router.js";
 import errorHandler from "./src/middlewares/errorHandler.mid.js";
@@ -22,6 +25,15 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(express.static(__dirname + "/public"));
 server.use(morgan("dev"));
+server.use(cookieParser());
+server.use(
+  sessions({
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: true,
+    store: new MongoStore({ mongoUrl: process.env.MONGO_URI, ttl: 60 * 60 }),
+  })
+);
 
 //endpoints
 server.use("/", indexRouter);
