d5e913cca48280cb7fe629b7a016fb2a44b877d8
igna <ignacioborraz@hotmail.com>
2024-05-13 22:13:33 -0300

    passport

--- 20_auth/src/routers/api/sessions.api.js
+++ 20_auth/src/routers/api/sessions.api.js
@@ -1,22 +1,16 @@
 import { Router } from "express";
 import usersManager from "../../data/mongo/UsersManager.mongo.js";
-import isValidData from "../../middlewares/isValidData.mid.js";
-import isValidEmail from "../../middlewares/isValidEmail.mid.js";
 import isValidUser from "../../middlewares/isValidUser.mid.js";
 import isValidPassword from "../../middlewares/isValidPasword.mid.js";
-import createHashPassword from "../../middlewares/createHashPassword.mid.js"
+import passport from "../../middlewares/passport.mid.js";
 
 const sessionsRouter = Router();
 
 sessionsRouter.post(
   "/register",
-  isValidData,
-  isValidEmail,
-  createHashPassword,
+  passport.authenticate("register", { session: false }),
   async (req, res, next) => {
     try {
-      const data = req.body;
-      await usersManager.create(data);
       return res.json({ statusCode: 201, message: "Registered!" });
     } catch (error) {
       return next(error);
@@ -25,17 +19,9 @@ sessionsRouter.post(
 );
 sessionsRouter.post(
   "/login",
-  isValidUser,
-  isValidPassword,
+  passport.authenticate("login", { session: false }),
   async (req, res, next) => {
     try {
-      const { email } = req.body;
-      const one = await usersManager.readByEmail(email);
-      req.session.email = email;
-      req.session.online = true;
-      req.session.role = one.role;
-      req.session.photo = one.photo;
-      req.session.user_id = one._id;
       return res.json({ statusCode: 200, message: "Logged in!" });
     } catch (error) {
       return next(error);
@@ -61,8 +47,13 @@ sessionsRouter.get("/online", async (req, res, next) => {
 });
 sessionsRouter.post("/signout", (req, res, next) => {
   try {
-    req.session.destroy();
-    return res.json({ statusCode: 200, message: "Signed out!" });
+    if (req.session.email) {
+      req.session.destroy();
+      return res.json({ statusCode: 200, message: "Signed out!" });
+    }
+    const error = new Error("Invalid credentials from signout");
+    error.statusCode = 401;
+    throw error;
   } catch (error) {
     return next(error);
   }
