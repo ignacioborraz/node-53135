0f7d3bfc44002af46f1a6252a702d4fd1a1f85c5
IGNACIO JAVIER BORRAZ <95113020+ignacioborraz@users.noreply.github.com>
2024-05-23 12:03:35 -0300

    23_router (#25)

---
+++ 23_router/src/middlewares/isTitle.mid.js
@@ -0,0 +1,15 @@
+function isTitle(req, res, next) {
+  try {
+    const { title } = req.body;
+    if (!title) {
+      const err = new Error("Insert title!");
+      err.statusCode = 400;
+      throw err;
+    }
+    return next();
+  } catch (error) {
+    return next(error);
+  }
+}
+
+export default isTitle;
