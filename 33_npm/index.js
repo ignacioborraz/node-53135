012d0ccadcb3ca075f14d97e2e570f17673bc975
igna <ignacioborraz@hotmail.com>
2024-07-03 14:28:41 -0300

    33_npm

---
+++ 33_npm/index.js
@@ -0,0 +1,4 @@
+import { multiplicacion } from "calculator-53135"
+
+const resultado1 = multiplicacion(2,10)
+console.log(resultado1)
---
+++ 33_npm/node_modules/calculator-53135/operaciones/resta.js
@@ -0,0 +1,3 @@
+const resta = (n1, n2) => n1 - n2;
+
+export default resta;
