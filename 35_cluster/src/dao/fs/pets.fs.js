1744754ec51dbca3e48aed65e11ef16e6ca24cd1
igna <ignacioborraz@hotmail.com>
2024-07-05 19:02:02 -0300

    app

---
+++ 35_cluster/src/dao/fs/pets.fs.js
@@ -0,0 +1,4 @@
+import Manager from "./manager.fs.js";
+
+const petsManager = new Manager("pets");
+export default petsManager;
---
+++ 35_cluster/src/services/service.js
@@ -0,0 +1,65 @@
+//SERVICIO LLAMA A REPOSITORIO
+
+class Service {
+  constructor(repository) {
+    this.repository = repository;
+  }
+  createService = async (data) => {
+    try {
+      const one = await this.repository.create(data);
+      return one;
+    } catch (error) {
+      throw error;
+    }
+  };
+  readAllService = async (filter) => {
+    try {
+      const all = await this.repository.read(filter);
+      return all;
+    } catch (error) {
+      throw error;
+    }
+  };
+  readByIdService = async (id) => {
+    try {
+      const one = await this.repository.readBy({ _id: id });
+      return one;
+    } catch (error) {
+      throw error;
+    }
+  };
+  readByEmailService = async (email) => {
+    try {
+      const one = await this.repository.readBy({ email });
+      return one;
+    } catch (error) {
+      throw error;
+    }
+  };
+  readByService = async (obj) => {
+    try {
+      const one = await this.repository.readById(obj);
+      return one;
+    } catch (error) {
+      throw error;
+    }
+  };
+  updateService = async (id, data) => {
+    try {
+      const one = await this.repository.update(id, data);
+      return one;
+    } catch (error) {
+      throw error;
+    }
+  };
+  destroyService = async (id) => {
+    try {
+      const one = await this.repository.destroy(id);
+      return one;
+    } catch (error) {
+      throw error;
+    }
+  };
+}
+
+export default Service;
