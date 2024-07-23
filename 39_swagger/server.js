f141f733f4fb9d5ce974f4dfd964c5479cfa996d
igna <ignacioborraz@hotmail.com>
2024-07-22 23:09:51 -0300

    39_swagger

--- 39_swagger/server.js
+++ 39_swagger/server.js
@@ -5,12 +5,16 @@ import cors from "cors";
 import compression from "express-compression";
 import express from "express"
 import { cpus } from "os";
-import argsUtil from "./src/utils/args.util.js";
+import swaggerJSDoc from "swagger-jsdoc";
+import { serve, setup } from "swagger-ui-express";
+
 
 import winston from "./src/middlewares/winston.mid.js";
 import indexRouter from "./src/routers/index.router.js";
 import errorHandler from "./src/middlewares/errorHandler.mid.js";
 import pathHandler from "./src/middlewares/pathHandler.mid.js";
+import argsUtil from "./src/utils/args.util.js";
+import configs from "./src/utils/swagger.util.js"
 import __dirname from "./utils.js";
 
 //http server
@@ -27,6 +31,7 @@ if(cluster.isPrimary) {
   console.log("proceso worker "+process.pid);
   server.listen(port, ready);
 }
+const specs = swaggerJSDoc(configs);
 
 //middlewares
 server.use(express.json());
@@ -35,6 +40,7 @@ server.use(express.static(__dirname + "/public"));
 server.use(winston);
 server.use(cookieParser(environment.SECRET_COOKIE));
 server.use(cors({ origin: true, credentials: true }));
+server.use("/api/docs", serve, setup(specs));
 server.use(
   compression({
     brotli: { enabled: true, zlib: {} },
--- 39_swagger/src/schemas/user.schema.js
+++ 39_swagger/src/schemas/user.schema.js
@@ -22,7 +22,7 @@ const usersSchema = joi.object({
       "string.min": "el email tiene que tener minimo 3 letras",
       "string.max": "el email tiene que tener maximo 50 letras",
     }),
-  password: joi.string().pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]+$')).min(3).max(50).required().messages({
+  password: joi.string().min(3).max(50).required().messages({
     "any.required": "la contraseña es requerido",
     "string.empty": "la contraseña no puede ser una cadena de texto vacía",
     "string.pattern.base": "la contraseña debe ser alfanumerica con min/MAY",
