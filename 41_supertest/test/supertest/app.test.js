ed17031eeaaf30ac70c0ebacd39f18ec50c3d934
igna <ignacioborraz@hotmail.com>
2024-07-29 22:16:08 -0300

    41_supertest

--- 41_supertest/test/supertest/app.test.js
+++ 41_supertest/test/supertest/app.test.js
@@ -1,68 +1,72 @@
-import environment from "../../src/utils/env.util.js";
 import { expect } from "chai";
 import supertest from "supertest";
-import dao from "../../src/dao/dao.factory.js";
-const { usersManager } = dao;
+import environment from "../../src/utils/env.util.js";
+import usersRepository from "../../src/repositories/auth.rep.js";
+import petsRepository from "../../src/repositories/pets.rep.js";
 
-const requester = supertest("http://localhost:" + environment.PORT + "/api");
+const requester = supertest(`http://localhost:${environment.PORT}/api`);
 
 describe("Testeando ADOPTME API", function () {
-  this.timeout(20000); // Aumenta el tiempo de espera para todas las pruebas dentro de este bloque
-
+  this.timeout(20000);
   const user = {
     first_name: "ignacio",
     email: "ignacioborraz@hotmail.com",
     password: "hola1234",
     role: "ADMIN",
-    verify: true
+    verify: true,
+  };
+  const animal = {
+    name: "tony",
+    specie: "dog",
   };
   let token = "";
-
-  it("Registro de un usuario correctamente", async function () {
-    try {
-      const response = await requester.post("/auth/register").send(user);
-      const { _body } = response
-      expect(_body.statusCode).to.be.equals(201);
-    } catch (error) {
-      console.error("Error en prueba de registro:", error);
-      throw error;
-    }
+  it("Registro de un usuario", async () => {
+    const response = await requester.post("/auth/register").send(user);
+    const { _body } = response;
+    //console.log(_body);
+    expect(_body.statusCode).to.be.equals(201);
   });
-
-  it("Inicio de sesión correctamente", async function () {
-    try {
-      const response = await requester.post("/auth/login").send(user);
-      const { headers } = response;
-      token = headers["set-cookie"][0].split(";")[0]
-      //console.log(token);
-      const { _body } = response
-      expect(_body.statusCode).to.be.equals(200);
-    } catch (error) {
-      console.error("Error en prueba de inicio de sesión:", error);
-      throw error;
-    }
+  it("Inicio de sesión de un usuario", async () => {
+    const response = await requester.post("/auth/login").send(user);
+    const { _body, headers } = response;
+    //console.log(_body);
+    //console.log(headers);
+    token = headers["set-cookie"][0].split(";")[0];
+    expect(_body.statusCode).to.be.equals(200);
   });
-
-  it("Cerrado de sesión correctamente", async function () {
-    try {
-      const response = await requester.post("/auth/signout").set("Cookie", token);
-      const { _body } = response
-      expect(_body.statusCode).to.be.equals(200);
-    } catch (error) {
-      console.error("Error en prueba de cerrado de sesión:", error);
-      throw error;
-    }
+  it("Creación de una mascota por parte de un administrador", async () => {
+    const response = await requester
+      .post("/pets")
+      .send(animal)
+      .set("Cookie", token);
+    const { _body } = response;
+    expect(_body.statusCode).to.be.equals(201);
   });
-
-  it("Eliminación de un usuario correctamente", async function () {
-    try {
-      const foundUser = await usersManager.readBy({ email: user.email });
-      const response = await requester.delete("/auth/" + foundUser._id);
-      const { _body } = response
-      expect(_body.statusCode).to.be.equals(200);
-    } catch (error) {
-      console.error("Error en prueba de eliminación de usuario:", error);
-      throw error;
-    }
+  it("Eliminación de una mascota por parte de un administrador", async () => {
+    const foundPet = await petsRepository.readBy({ name: animal.name });
+    const response = await requester
+      .delete("/pets/" + foundPet._id)
+      .set("Cookie", token);
+    const { _body } = response;
+    expect(_body.statusCode).to.be.equals(200);
+  });
+  it("Eliminación de una mascota sin haber iniciado sesión", async () => {
+    const response = await requester
+      .delete("/pets/666a384a3e5c8e385e8b90d8")
+      .send(animal);
+    const { _body } = response;
+    //console.log(_body);
+    expect(_body.statusCode).to.be.equals(401);
+  });
+  it("Cerrado de sesión", async () => {
+    const response = await requester.post("/auth/signout").set("Cookie", token);
+    const { _body } = response;
+    expect(_body.statusCode).to.be.equals(200);
+  });
+  it("Eliminación de un usuario", async () => {
+    const foundUser = await usersRepository.readByEmail(user.email);
+    const response = await requester.delete("/auth/" + foundUser._id);
+    const { _body } = response;
+    expect(_body.statusCode).to.be.equals(200);
   });
 });
